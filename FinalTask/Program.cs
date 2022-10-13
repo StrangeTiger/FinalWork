a247fd80962307c807bac8d9fb29ad0a727ba353
Evgeniia_Makarova <evgetigra@gmail.com>
2022-10-13 20:24:20 +0300

    Добавлено решение задачи

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] array = new string[]  {};
+int n = InputNumbers("Введите количество элементов массива: ");
+array = new string[n];
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($" Введите {i+1} строку: ");
+  array[i] = Console.ReadLine();
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+string[] СharacterСounter(string[] array)
+{
+ string[] newArray = new string[n];
+  int count = 0;
+    for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i].Length <= 3)
+        {
+          newArray[count] = array[i];
+          count++;
+        }
+    }
+    Array.Resize(ref newArray, count);
+    return newArray;
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
+
+PrintArray (СharacterСounter(array));
+
